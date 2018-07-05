819e1d24b65913e86053ed97742e064417f6c4c8
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2018-07-05 02:16:25 -0300

    Adding TodoList and Task models

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todolists.apps.TodolistsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- todolists/models.py
+++ todolists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    list_title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+
+class Task(models.Model):
+    todolist = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    task_text = models.CharField(max_length=200)
+    isCompleted = models.BooleanField(default='false')
+    deadline = models.DateField('deadline date')
+    # include here a variable for related users later
